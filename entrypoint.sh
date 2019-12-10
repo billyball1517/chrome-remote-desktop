6354e882946524a580fa10cd7ce088442332b6b1
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2019-12-09 19:59:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Creating user with UID : $USER_ID"
 useradd -m -s /bin/bash -u $USER_ID -o -G chrome-remote-desktop user
 echo "#!/bin/bash" > /home/user/.chrome-remote-desktop-session
-echo "/usr/bin/startlxde" >> /home/user/.chrome-remote-desktop-session
+echo "exec /usr/bin/startlxde" >> /home/user/.chrome-remote-desktop-session
 
 service chrome-remote-desktop start
 
