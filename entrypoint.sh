0da500cd17674530b02baa8401afb0219e5ac473
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2019-12-15 01:11:23 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,6 @@ echo "Creating user with UID : $USER_ID"
 useradd -m -s /bin/bash -u $USER_ID -o -G chrome-remote-desktop user
 export HOME=/home/user
 
+service chrome-remote-desktop start
+
 exec /usr/sbin/gosu user "$@"
